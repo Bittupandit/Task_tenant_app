7576df169c8bf13420ae931d0d3b92d995eb38f2
Surendra Mishra <surendra.mishra@arohatech.com>
2021-05-22 22:28:33 +0530

    Tenant App

---
+++ Task/Tenant/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+# Create your models here.
+
+# Tenant App => Create Monthly Expenditure | Fetch the data form db | Update the data for correction | Delete Date if wrong.
